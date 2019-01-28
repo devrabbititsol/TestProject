cbf719d0c656142e1ae76baf9f6ef2745b39ea8f
Rajsekhar355 <rajasekhar.alimili@devrabbit.com>
2019-01-28 11:02:46 +0530

    first commit

---
+++ src/test/java/web/testproject1/devrabbittest/webpageclasses/TestScreen.java
@@ -0,0 +1,33 @@
+package web.testproject1.devrabbittest.webpageclasses;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import com.utilities.BaseClass;import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+@SuppressWarnings("unused")
+public class TestScreen extends BaseClass {
+	
+	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"http://www.devrabbit.com/\",\"report_upload_url\":\"https://192.168.1.142:8080/TAF_Automation_DR/UploadReportFile\",\"project_name\":\"TestProject1\",\"project_description\":\"\",\"project_id\":206,\"module_name\":\"DevrabbitTest\",\"module_description\":\"\",\"sub_module_id\":0,\"module_id\":349,\"testcase_name\":\"TC_HomePage\",\"testcase_id\":414,\"testset_id\":0,\"executed_timestamp\":-1829630268,\"browser_type\":\"chrome\"}";
+
+	public static String projectName = "testproject1";
+	public WebDriver driver;
+	public ExtentReports reports;
+	public ExtentTest test;
+	public static final int datasetsLength = 1;
+
+	public TestScreen(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	@FindBy(how = How.XPATH, using = "//LI[@id='menu-item-1476']/A[1]")	
+	private WebElement	Contact_292647A;
+	public String clkAContact_292647() {
+		waitForExpectedElement(driver, Contact_292647A);		
+		String text = Contact_292647A.getText();
+		Contact_292647A.click();
+		return text;
+	}
+
+}
---
+++ src/test/java/web/testproject1/devrabbittest/webtestclasses/TC_HomePage.java
@@ -0,0 +1,80 @@
+package web.testproject1.devrabbittest.webtestclasses;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.configurations.Constants;
+import com.configurations.ExtentConfigurations;
+import web.testproject1.devrabbittest.webpageclasses.TestScreen;
+import com.utilities.BaseClass;
+import com.utilities.ConfigFilesUtility;
+import com.utilities.Utilities;
+import org.json.JSONObject;
+
+@SuppressWarnings("unused")
+public class TC_HomePage extends BaseClass {
+	ExtentReports reports;
+	ExtentTest test;
+	ITestResult result;
+	private Logger logger;
+	private ConfigFilesUtility configFileObj;
+	public boolean isElementDispalyed = false;	public static final int datasetsLength = 1;
+
+	public TC_HomePage() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+		logger = Logger.getLogger(TC_HomePage.class);
+		configFileObj = new ConfigFilesUtility();
+		configFileObj.loadPropertyFile("tc_homepage.properties");
+		reports = ExtentConfigurations.getExtentInstance(reportsPath, projectPath, TestScreen.projectName);
+		test = reports.startTest("TC_HomePage");
+	}
+	
+	public void setUP() throws Exception {
+		String primaryInfo = TestScreen.primaryInfo;
+		Constants.PRIMARY_INFO = primaryInfo;
+		driver = launchBrowser(new JSONObject(primaryInfo).optString("browser_type"), configFileObj);
+	}
+
+	public void TestScreenTest(int i) throws Exception {
+
+	 try{
+		Constants.TOTAL_TC = Constants.TOTAL_TC + 1;
+		Constants.IS_TESTCASE = true; Constants.iS_WEB = true;
+		int datasetScreencount = 1;
+		TestScreen objTestScreen = PageFactory.initElements(driver, TestScreen.class);
+		testLogHeader(test, "Verify TestScreen page");
+String text1 = objTestScreen.clkAContact_292647();
+		if(text1.equalsIgnoreCase(configFileObj.getProperty("Contact"+ i + datasetScreencount))){
+			printSuccessLogAndReport(test, logger,  "Clicked on : " + configFileObj.getProperty("Contact"+ i + datasetScreencount));
+			printSuccessLogAndReport(test, logger,  "Validated Link Text : " + configFileObj.getProperty("Contact"+ i + datasetScreencount));
+		} else {
+			printFailureLogAndReport(test, logger,  "Link Text is not displayed  : " + configFileObj.getProperty("Contact"+ i + datasetScreencount));
+		}
+
+	   } catch (Exception e) {
+		  isElementDispalyed = false;
+		  printFailureLogAndReport(test, logger,  "Element is not found" + e.getLocalizedMessage());
+		}
+	}
+	
+	@Test
+	public void screensTest() throws Exception {
+		isElementDispalyed = true;
+		for(int datasets = 1; datasets <= TestScreen.datasetsLength; datasets++) {
+			if(!isElementDispalyed) return;			
+			setUP();
+			if(isElementDispalyed) { TestScreenTest(datasets);}
+			tearDown();
+		}	}
+
+	
+	public void tearDown() throws Exception {
+		reports.endTest(test);
+		reports.flush();
+		driver.quit();
+	}
+}
